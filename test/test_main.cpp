eec946c77a4ee3e080c6676b80d44dcf592372e9
TSprech <trey.sprecher@gmail.com>
2022-05-22 18:19:25 -0400

    Reenabled All Warnings
    Reenabled the compiler warnings as errors and removed excess lines in test_main.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -4,21 +4,7 @@
 
 #include "gtest/gtest.h"
 
-#include <charconv>
-#include <numeric>
-
-#include "usf/develop/usf_config.hpp"
-#include "usf/develop/usf_traits.hpp"
-#include "usf/develop/usf_integer.hpp"
-#include "usf/develop/usf_float.hpp"
-#include "usf/develop/usf_arg_format.hpp"
-#include "usf/develop/usf_arg_custom_type.hpp"
-#include "usf/develop/usf_argument.hpp"
-#include "usf/develop/usf_main.hpp"
-
 int main(int ac, char* av[]) {
-  std::array<char, 50> str{};
-//  std::cout << str.data() << std::endl;
   testing::InitGoogleTest(&ac, av);
   return RUN_ALL_TESTS();
 }
